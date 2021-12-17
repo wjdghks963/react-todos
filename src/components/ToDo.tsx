6e2c1d686c019d870b549572ffd9ee7c99ef6c6a
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-12-17 21:27:11 +0900

    선택한 todo의 index를 object의 id를 이용해 찾음

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,20 +1,39 @@
+import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  const onClick = (newCategory: IToDo["category"]) => {};
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      // 선택한 타켓과 toDo의 id가 같은지 확인
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      return oldToDos;
+    });
+  };
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>To Do</button>
+        <button name="DOING" onClick={onClick}>
+          To Do
+        </button>
       )}
       {category !== "TO_DO" && (
-        <button onClick={() => onClick("TO_DO")}>Doing</button>
+        <button name="TO_DO" onClick={onClick}>
+          Doing
+        </button>
       )}
       {category !== "DONE" && (
-        <button onClick={() => onClick("DONE")}>Done</button>
+        <button name="DONE" onClick={onClick}>
+          {" "}
+          Done
+        </button>
       )}
     </li>
   );
